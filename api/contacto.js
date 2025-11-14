0361b7a265b164f5b4b56893c400fe1bede1fe73
Lopo <lopodo@yumly.es>
2025-11-14 09:47:25 +0100

    cambios de contacto

--- api/contacto.js
+++ api/contacto.js
@@ -24,7 +24,7 @@ export default async function handler(req, res) {
     try {
       await resend.emails.send({
         from: 'Formulario Web <info@yumly.es>',
-        to: 'info@zoho.com',
+        to: 'info@zoho.cominfo@yumly.es',
         subject: 'Nuevo mensaje desde tu web',
         html: `
           <h2>Nuevo mensaje recibido</h2>
