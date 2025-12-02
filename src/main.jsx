8ccedb9f804b8a01356719d84f5611995f3907d9
lopodo <lopodo@yumly.es>
2025-12-02 18:40:51 +0100

    eliminar sobras

--- src/main.jsx
+++ src/main.jsx
@@ -1,19 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import { Analytics } from '@vercel/analytics/react'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.jsx";
+import { Analytics } from "@vercel/analytics/react";
 ///////////////////
 //hoja de estilos//
 ///////////////////
-import './styles/index.css'
-import './styles/App.css'
-import './styles/ordenador.css'
-import './styles/tablet.css'
-import './styles/mobile.css'
+import "./styles/index.css";
+import "./styles/App.css";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
     <App />
-    <Analytics/>
-  </StrictMode>,
-)
+    <Analytics />
+  </StrictMode>
+);
